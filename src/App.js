1f656031e8c1e8c78d19851cddbbea2cb0fa8473
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-08-03 22:09:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import ContactsEdit from "./components/ContactsEdit"
-import Meetings from "./components/Meetings"
-import client from './utils/client.js'
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import ContactsEdit from "./components/ContactsEdit";
+import Meetings from "./components/Meetings";
+import client from "./utils/client.js";
 
-import "./styles/styles.css"
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(async () => {
-    const data = await client.get('/contacts')
-    setContacts(data.contacts)
-    setIsLoading(false)
-  }, [])
+    const data = await client.get("/contacts");
+    console.log("contacts:", data);
+    setContacts(data);
+    setIsLoading(false);
+  }, []);
 
   return (
     <>
       <nav>
         <h2>Menu</h2>
         <ul>
-          <li><Link to='/'>Contacts List</Link></li>
-          <li><Link to='/contacts/add'>Add New Contact</Link></li>
+          <li>
+            <Link to="/">Contacts List</Link>
+          </li>
+          <li>
+            <Link to="/contacts/add">Add New Contact</Link>
+          </li>
         </ul>
       </nav>
       <main>
         <Routes>
-          <Route path='/' element={<ContactsList contacts={contacts} setContacts={setContacts} isLoading={isLoading}/>} />
-          <Route path='/contacts/add' element={<ContactsAdd setContacts={setContacts} contacts={contacts}/>} />
-          <Route path='/contacts/:id' element={<ContactsView />} />
-          <Route path='/contacts/:id/edit' element={<ContactsEdit setContacts={setContacts} contacts={contacts}/>} />
-          <Route path='/contacts/:id/meetings' element={<Meetings />} />
+          <Route
+            path="/"
+            element={
+              <ContactsList
+                contacts={contacts}
+                setContacts={setContacts}
+                isLoading={isLoading}
+              />
+            }
+          />
+          <Route
+            path="/contacts/add"
+            element={
+              <ContactsAdd setContacts={setContacts} contacts={contacts} />
+            }
+          />
+          <Route path="/contacts/:id" element={<ContactsView />} />
+          <Route
+            path="/contacts/:id/edit"
+            element={
+              <ContactsEdit setContacts={setContacts} contacts={contacts} />
+            }
+          />
+          <Route path="/contacts/:id/meetings" element={<Meetings />} />
         </Routes>
       </main>
     </>
-  )
+  );
 }
